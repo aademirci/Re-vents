a1c426d484d13b552acdaea84efbbfc715c9c043
A.A. Demirci <aalperdemirci@gmail.com>
2021-03-08 23:44:52 +0300

    End of Section 12

--- src/features/nav/SignedInMenu.jsx
+++ src/features/nav/SignedInMenu.jsx
@@ -6,7 +6,7 @@ import { Dropdown, Image, Menu } from 'semantic-ui-react'
 import { signOutFirebase } from '../../app/firestore/firebaseService'
 
 const SignedInMenu = () => {
-    const { currentUser } = useSelector(state => state.auth)
+    const { currentUserProfile } = useSelector(state => state.profile)
     const history = useHistory()
 
     const handleSignOut = async () => {
@@ -20,11 +20,11 @@ const SignedInMenu = () => {
 
     return (
         <Menu.Item position="right">
-            <Image avatar spaced="right" src={currentUser.photoURL || '/assets/user.png'} />
-            <Dropdown pointing="top left" text={currentUser.displayName}>
+            <Image avatar spaced="right" src={currentUserProfile.photoURL || '/assets/user.png'} />
+            <Dropdown pointing="top left" text={currentUserProfile.displayName}>
                 <Dropdown.Menu>
                     <Dropdown.Item as={Link} to="/create" text="Create event" icon="plus" />
-                    <Dropdown.Item text="My profile" icon="user" />
+                    <Dropdown.Item as={Link} to={`/profile/${currentUserProfile.id}`} text="My profile" icon="user" />
                     <Dropdown.Item text="My account" icon="settings" as={Link} to="/account" />
                     <Dropdown.Item text="Sign out" icon="power" onClick={handleSignOut} />
                 </Dropdown.Menu>
